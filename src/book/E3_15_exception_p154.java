b8d21f19d7b8eb0b88555e936ebbb4615d3722cb
noory <noory@DESKTOP-R15MO1Q>
2022-12-26 20:59:49 +0900

    p155

---
+++ src/book/E3_15_exception_p154.java
@@ -0,0 +1,25 @@
+package book;
+
+import java.util.Scanner;
+
+public class E3_15_exception_p154 {
+	// 정수를 0으로 나누는 경우 catch/try 문
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("나뉨수를 입력하시오 : ");
+			int dividend = scanner.nextInt(); // 나뉨수 입력
+			System.out.println("나눔수를 입력하시오 : ");
+			int divisor = scanner.nextInt(); // 나눗수 입력
+
+			try {
+				System.out.println(dividend + "를 " + divisor + "로 나누면 몫은 " + dividend / divisor + "입니다.");
+				break; // 정상적인 나누기 완료 후 while 벗어나기
+			} catch (ArithmeticException e) { // ArithmeticException 예외 처리 코드
+				System.out.println("0으로 나눌 수 없습니다! 다시 입력하세요");
+			}
+		}
+		scanner.close();
+	}
+}
